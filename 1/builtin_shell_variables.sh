3b1dde73dbae36e48e332339b2f8f80550543470
rahymov <y.rahymov@gmail.com>
2021-10-20 08:09:46 -0400

    added builtin func, path vars checker

---
+++ 1/builtin_shell_variables.sh
@@ -0,0 +1,6 @@
+echo "My current bash path - $BASH"
+echo "Bash version I am using - $BASH_VERSION"
+echo "PID of bash I am running - $BASHPID"
+echo "My home directory - $HOME"
+echo "Where am I currently? - $PWD"
+echo "My hostname - $HOSTNAME"
